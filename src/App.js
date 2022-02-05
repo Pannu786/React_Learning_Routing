570749a868e59f1737d64504a599ac3339967773
Pannu786 <thepannu786@gmail.com>
2022-02-05 15:50:29 +0530

    basic Route for pages dne

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Route, Switch } from 'react-router-dom';
+import AllQuotes from './pages/AllQuotes';
+import NewQuotes from './pages/NewQuotes';
+import QuotesDetail from './pages/QuotesDetail';
+
 function App() {
   return (
-    <div>
-<h1>H</h1>
-    </div>
+    <Switch>
+      <Route path='/quotes' exact>
+        <AllQuotes />
+      </Route>
+      <Route path='/quotes/:quoteId'>
+        <QuotesDetail />
+      </Route>
+      <Route path='/new-quotes'>
+        <NewQuotes />
+      </Route>
+    </Switch>
   );
 }
 
---
+++ src/pages/QuotesDetail.js
@@ -0,0 +1,5 @@
+const QuotesDetail = () => {
+  return <h1>Quote Detail Page</h1>;
+};
+
+export default QuotesDetail;
