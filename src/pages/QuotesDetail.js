1fa7d9c00db8c6d4cc8c8c1428562cce5953ab3f
Pannu786 <thepannu786@gmail.com>
2022-02-06 14:31:01 +0530

    Dummy data added

--- src/pages/QuotesDetail.js
+++ src/pages/QuotesDetail.js
@@ -1,19 +1,27 @@
-
 import { useParams, Route } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Pannu', text: 'This is Awesome' },
+  { id: 'q2', author: 'Pannu JI', text: 'This is Awesome Braaa' },
+];
 
 const QuotesDetail = () => {
   const params = useParams();
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>Quote not found</p>;
+  }
 
   return (
     <>
-      <h1>Quote Detail Page</h1>
-      <p>Quote ID: {params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
     </>
   );
 };
-
 export default QuotesDetail;
